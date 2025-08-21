caa2ff6d740853af4cfcc4493a2bef7972d620cc
Franco Morales Castro <franco.morales@outlook.com>
2025-08-20 21:44:31 -0400

    feat: add model

--- misitio/portafolio/models.py
+++ misitio/portafolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Projects:
+    def __init__(self, title: str, description: str, draft: bool = True):
+        self.title = title
+        self.description = description
+        self.draft = draft
+
+    def is_draft(self):
+        return self.draft
+    
+PROJECTS_DATA = [
+    Projects("Proyecto mis animalitos", "Aplicacion para gestión de un zoológico.", False),
+    Projects("Proyecto de reciclaje", "Aplicación para promover el reciclaje en la comunidad.", True),
+    Projects("Proyecto de agricultura urbana", "Aplicación para la gestión de huertos urbanos.", False),
+]
--- misitio/portafolio/views.py
+++ misitio/portafolio/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Projects, PROJECTS_DATA
 
 # Create your views here.
 def portafolio_home(request):
-    return render(request, 'portafolio_home.html')
+    projects_not_draft = []
+    for project in PROJECTS_DATA:
+        if not project.is_draft():
+            projects_not_draft.append(project)
+    #
+    # projects_not_draft = [
+    #     a for a in PROJECTS_DATA if not a.is_draft()
+    # ]
+    #
+
+    return render(request, 'portafolio_home.html', { 'projects': projects_not_draft })
